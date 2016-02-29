951e5813d1f06b140f1c133305a829aec423d335
Manuel Vargas Alegria <manuel.vargas.alegria@gmail.com>
2016-02-29 10:54:16 -0500

    Update maps_lib.js

--- empresas/source/maps_lib.js
+++ empresas/source/maps_lib.js
@@ -138,8 +138,8 @@ templateId: 4
         select: MapsLib.locationColumn,
         where:  whereClause
       },
-      styleId: 2,
-      templateId: 2
+      styleId: 3,
+      templateId: 5
     });
     MapsLib.searchrecords.setMap(map);
     MapsLib.getCount(whereClause);
